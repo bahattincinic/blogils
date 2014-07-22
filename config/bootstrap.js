2e538520ad0477cddf8bb33f6920b0bf44a504bd
Bahattin Cinic <bahattincinic@gmail.com>
2014-07-22 12:34:17 +0300

    change bootsrap settings

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,16 +10,35 @@
 
 module.exports.bootstrap = function (cb) {
 
-  config.bloggerName = "Bahattin Cinic";
-  config.subTitle = "Developer";
-  config.biography = "Trust yourself! You can do it!";
-  config.avatar = "https://secure.gravatar.com/avatar/c1184fefac22e49bbf59e3775ef6e9dd.png?size=400";
-  config.facebook = "https://facebook.com/bahattincinic/";
-  config.twitter = "https://twitter.com/bahattincinic/";
-  config.github = "https://github.com/bahattincinic/";
-  config.email = "bahattincinic@gmail.com";
+  /* Blogger Settings */
+  config.blogger = {
+    name: 'Bahattin Cinic',
+    subTitle: 'Developer',
+    biography: 'Trust yourself! You can do it!',
+    avatar: 'https://secure.gravatar.com/avatar/c1184fefac22e49bbf59e3775ef6e9dd.png?size=400'
+  }
 
-  // It's very important to trigger this callack method when you are finished 
+  /* Blogger Social Accounts */
+  config.accounts = [
+    {
+      title: 'facebook',
+      link: 'https://facebook.com/bahattincinic/'
+    },
+    {
+      title: 'twitter',
+      link: 'https://twitter.com/bahattincinic/'
+    },
+    {
+      title: 'github',
+      link: 'https://github.com/bahattincinic/',
+    },
+    {
+      title: 'mail',
+      link: 'mailto:bahattincinic@gmail.com'
+    }
+  ];
+
+  // It's very important to trigger this callack method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   cb();
 };
