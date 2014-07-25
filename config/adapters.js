b732f59699a5645746d360f9cd058db725acfc94
Bahattin Cinic <bahattincinic@gmail.com>
2014-07-26 02:01:04 +0300

    change package.json

--- config/adapters.js
+++ config/adapters.js
@@ -16,5 +16,24 @@ module.exports.adapters = {
 
   // If you leave the adapter config unspecified
   // in a model definition, 'default' will be used.
+  'default': 'disk',
 
+  // Persistent adapter for DEVELOPMENT ONLY
+  // (data is preserved when the server shuts down)
+  disk: {
+    module: 'sails-disk'
+  },
+
+  // MySQL is the world's most popular relational database.
+  // Learn more: http://en.wikipedia.org/wiki/MySQL
+  myLocalMySQLDatabase: {
+
+    module: 'sails-mysql',
+    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
+    user: 'YOUR_MYSQL_USER',
+    // Psst.. You can put your password in config/local.js instead
+    // so you don't inadvertently push it up if you're using version control
+    password: 'YOUR_MYSQL_PASSWORD',
+    database: 'YOUR_MYSQL_DB'
+  }
 };
